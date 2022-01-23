4caae1a108f1e354d19d0fa47c315fd17b73b71a
victorsbandeira <94652859+victorsbandeira@users.noreply.github.com>
2022-01-23 11:46:52 -0300

    Add files via upload

---
+++ Aula 11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel} KM/H`)
+    if(vel > 60){
+        console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+    }
+    else{
+console.log("Dirija sempre usando cinto de segurança!")
+     }
